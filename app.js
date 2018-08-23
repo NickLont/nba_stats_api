768338546cb97d770c018a6d1e70d6bdd7c3e72f
nicklontorfos <nlontorfos@hotmail.com>
2018-08-23 10:58:23 +0200

    added Authorization header solving CORS error

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(responseTime())
 // })
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
 
